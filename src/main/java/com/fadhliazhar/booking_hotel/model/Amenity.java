f016aec0b36fda33367799d57122b427f2a58dc8
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-08-07 21:35:00 +0700

    - Add enum Amenity - Add enum RoomServiceType

---
+++ src/main/java/com/fadhliazhar/booking_hotel/model/Amenity.java
@@ -0,0 +1,10 @@
+package com.fadhliazhar.booking_hotel.model;
+
+public enum Amenity {
+    TELEVISION,
+    INTERNET_ACCESS,
+    HAIR_DRYER,
+    PREMIUM_TOWELS,
+    CRIB,
+    SAFE
+}
---
+++ src/main/java/com/fadhliazhar/booking_hotel/model/RoomServiceType.java
@@ -0,0 +1,5 @@
+package com.fadhliazhar.booking_hotel.model;
+
+public enum RoomServiceType {
+    BIR, WHISKY
+}
